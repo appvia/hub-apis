dc1a9e011f3eaed5420753be93dfde77893a18a3
Rohith <gambol99@gmail.com>
2019-11-29 14:09:01 +0000

    - adding a check on crd registration

--- pkg/publish/crds.go
+++ pkg/publish/crds.go
@@ -18,6 +18,10 @@
 package publish
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	client "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -44,21 +48,76 @@ func ApplyCustomResourceDefinitions(c client.Interface, list []*apiextensions.Cu
 // ApplyCustomResourceDefinition is responsible for applying the CRD to the cluster
 func ApplyCustomResourceDefinition(c client.Interface, crd *apiextensions.CustomResourceDefinition) error {
 	// @step: retrieve the current if there
-	current, err := c.ApiextensionsV1beta1().CustomResourceDefinitions().Get(crd.Name, metav1.GetOptions{})
-	if err != nil {
-		if !errors.IsNotFound(err) {
+	err := func() error {
+		current, err := c.ApiextensionsV1beta1().CustomResourceDefinitions().Get(crd.Name, metav1.GetOptions{})
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				return err
+			}
+
+			_, err := c.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+
 			return err
 		}
 
-		_, err := c.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+		crd.SetGeneration(current.GetGeneration())
+		crd.SetResourceVersion(current.GetResourceVersion())
 
+		_, err = c.ApiextensionsV1beta1().CustomResourceDefinitions().Update(crd)
+
+		return err
+	}()
+	if err != nil {
 		return err
 	}
 
-	crd.SetGeneration(current.GetGeneration())
-	crd.SetResourceVersion(current.GetResourceVersion())
+	return CheckCustomResourceDefinition(c, crd)
+}
 
-	_, err = c.ApiextensionsV1beta1().CustomResourceDefinitions().Update(crd)
+// CheckCustomResourceDefinition ensures the CRD is ok to go
+func CheckCustomResourceDefinition(c client.Interface, crd *apiextensions.CustomResourceDefinition) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	return err
+	doneCh := make(chan struct{}, 0)
+
+	go func() {
+		for {
+			select {
+			case _ = <-ctx.Done():
+				return
+			default:
+			}
+
+			err := func() error {
+				// @step: ensure the crd has been registered
+				obj, err := c.ApiextensionsV1beta1().CustomResourceDefinitions().Get(crd.Name, metav1.GetOptions{})
+				if err != nil {
+					return err
+				}
+				if len(obj.Status.Conditions) < 2 {
+					return fmt.Errorf("waiting for crd conditions to reach 2")
+				}
+				for _, x := range obj.Status.Conditions {
+					if x.Status != "True" {
+						return fmt.Errorf("condition not met, reason: %s", x.Reason)
+					}
+				}
+				time.Sleep(200 * time.Millisecond)
+
+				return nil
+			}()
+			if err == nil {
+				doneCh <- struct{}{}
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-doneCh:
+		return nil
+	case <-time.After(10 * time.Second):
+		return fmt.Errorf("failed to register the crd")
+	}
 }
